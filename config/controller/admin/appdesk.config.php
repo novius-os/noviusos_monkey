3682f9b5cc9eb081e4b5211d70e3d865e2060f34
Gilles FELIX <felix@novius.com>
2012-08-14 17:02:49 +0200

    Refactoring appdesk conf json into php

--- config/controller/admin/appdesk.config.php
+++ config/controller/admin/appdesk.config.php
@@ -23,7 +23,6 @@ return array(
 	'views' => array(
 		'default' => array(
 			'name' => __('Default view'),
-			'json' => array('static/apps/noviusos_monkey/js/admin/monkey.js'),
 		),
 	),
 	'dataset' => array(
@@ -54,4 +53,149 @@ return array(
 			return $query;
 		},
 	),
+    'appdesk' => array(
+        'tab' => array(
+            'label' => __('Monkey'),
+            'iconUrl' => 'static/apps/noviusos_monkey/img/32/monkey.png'
+        ),
+        'actions' => array(
+            'update' => array(
+                'action' => array(
+                    'action' => 'nosTabs',
+                    'tab' => array(
+                        'url' => "admin/noviusos_monkey/monkey/insert_update/{{id}}",
+                        'label' => __('Edit'),
+                    ),
+                ),
+                'label' => __('Edit'),
+                'name' => 'edit',
+                'primary' => true,
+                'icon' => 'pencil'
+            ),
+            'delete' => array(
+                'action' => array(
+                    'action' => 'confirmationDialog',
+                    'dialog' => array(
+                        'contentUrl' => 'admin/noviusos_monkey/monkey/delete/{{id}}',
+                        'title' => __('Delete a monkey'),
+                    ),
+                ),
+                'label' => __('Delete'),
+                'name' => 'delete',
+                'primary' => true,
+                'icon' => 'trash'
+            ),
+            'visualise' => array(
+                'label' => 'Visualise',
+                'name' => 'visualise',
+                'primary' => true,
+                'iconClasses' => 'nos-icon16 nos-icon16-eye',
+                'action' => array(
+                    'action' => 'window.open',
+                    'url' => '{{url}}?_preview=1'
+                ),
+            ),
+        ),
+        'reloadEvent' => 'Nos\\Monkey\\Model_Monkey',
+        'appdesk' => array(
+            'adds' => array(
+                'monkey' => array(
+                    'label' => __('Add a monkey'),
+                    'action' => array(
+                        'action' => 'nosTabs',
+                        'method' => 'add',
+                        'tab' => array(
+                            'url' => 'admin/noviusos_monkey/monkey/insert_update?lang={{lang}}',
+                            'label' => __('Add a new monkey'),
+                        ),
+                    ),
+                ),
+                'species' => array(
+                    'label' => __('Add a species'),
+                    'action' => array(
+                        'action' => 'nosTabs',
+                        'method' => 'add',
+                        'tab' => array(
+                            'url' => 'admin/noviusos_monkey/species/insert_update?lang={{lang}}',
+                            'label' => 'Add a species'
+                        ),
+                    ),
+                ),
+            ),
+            'splittersVertical' => 250,
+            'grid' => array(
+                'proxyUrl' => 'admin/noviusos_monkey/appdesk/json',
+                'columns' => array(
+                    'name' => array(
+                        'headerText' => __('Name'),
+                        'dataKey' => 'name'
+                    ),
+                    'lang' => array(
+                        'lang' => true
+                    ),
+                    'species' => array(
+                        'headerText' => __('Species'),
+                        'dataKey' => 'species'
+                    ),
+                    'published' => array(
+                        'headerText' => __('Status'),
+                        'dataKey' => 'publication_status'
+                    ),
+                    'actions' => array(
+                        'actions' => ['update', 'delete', 'visualise']
+                    ),
+                ),
+            ),
+            'inspectors' => array(
+                'speciess' => array(
+                    'reloadEvent' => 'Nos\\Monkey\\Model_Species',
+                    'label' => __('Speciess'),
+                    'url' => 'admin/noviusos_monkey/inspector/species/list',
+                    'grid' => array(
+                        'columns' => array(
+                            'title' => array(
+                                'headerText' => __('Species'),
+                                'dataKey' => 'title'
+                            ),
+                            'actions' => array(
+                                'showOnlyArrow' => true,
+                                'actions' => [
+                                    array(
+                                        'action' => array(
+                                            'action' => 'nosTabs',
+                                            'tab' => array(
+                                                'url' => "admin/noviusos_monkey/species/insert_update/{{id}}",
+                                                'label' => __('Edit'),
+                                            ),
+                                        ),
+                                        'label' => __('Edit'),
+                                        'name' => 'edit',
+                                        'primary' => true,
+                                        'icon' => 'pencil'
+                                    ),
+                                    array(
+                                        'action' => array(
+                                            'action' => 'confirmationDialog',
+                                            'dialog' => array(
+                                                'contentUrl' => 'admin/noviusos_monkey/species/delete/{{id}}',
+                                                'title' => __('Delete a species'),
+                                            ),
+                                        ),
+                                        'label' => __('Delete'),
+                                        'name' => 'delete',
+                                        'primary' => true,
+                                        'icon' => 'trash'
+                                    ),
+                                ]
+                            ),
+
+                        ),
+                        'urlJson' => 'admin/noviusos_monkey/inspector/species/json'
+                    ),
+                    'inputName' => 'monk_species_id[]',
+                    'vertical' => true
+                ),
+            ),
+        ),
+    ),
 );

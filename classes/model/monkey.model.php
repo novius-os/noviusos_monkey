8df8a922d25f5f8a1b3b26c90248901d5ea3d392
Gilles FELIX <gilles@famille-felix.fr>
2013-04-12 10:08:17 +0300

    Test scrutinizer notification

--- classes/model/monkey.model.php
+++ classes/model/monkey.model.php
@@ -10,8 +10,7 @@
 
 namespace Nos\Monkey;
 
-class Model_Monkey extends \Nos\Orm\Model
-{
+class Model_Monkey extends \Nos\Orm\Model{
     protected static $_table_name = 'nos_monkey';
     protected static $_primary_key = array('monk_id');
 

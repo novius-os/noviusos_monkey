0a552e573506b18f3bdd93d8d7319773d2d0d953
Sébastien Drouyer <drouyer@novius.com>
2013-04-24 18:02:10 +0200

    monkey

--- migrations/003_version_0_2_1.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Nos\Monkey\Migrations;
-
-class Version_0_2_1 extends \Nos\Migration
-{
-}
--- migrations/003_version_0_2_1.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `nos_monkey`
-  ADD `monk_publication_start` DATETIME NULL DEFAULT NULL AFTER `monk_published` ,
-  ADD `monk_publication_end` DATETIME NULL DEFAULT NULL AFTER `monk_publication_start`;

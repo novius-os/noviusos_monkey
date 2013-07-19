336a1fa1e3737e4fcb37938fec272c7168bbfe9b
Gilles FELIX <felix@novius.com>
2013-07-19 11:32:10 +0200

    "call of Model_Media getImgTag and getImagTagResized change by htmlImg and htmlImgResized"

--- views/front/fields.view.php
+++ views/front/fields.view.php
@@ -17,7 +17,7 @@ if (!empty($display['summary']) && !empty($item->monk_summary)) {
 }
 
 if (!empty($display['thumbnail']) && !empty($item->medias->thumbnail)) {
-    echo $item->medias->thumbnail->getImgTagResized(200, null, array('alt' => $item->monk_name));
+    echo $item->medias->thumbnail->htmlImgResized(200, null, array('alt' => $item->monk_name));
 }
 
 if (!empty($display['wysiwyg']) && !empty($item->wysiwygs)) {

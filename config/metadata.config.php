4146f08ba77213e2e3d9d9b517d0a3e13fcd327a
Gilles FELIX <felix@novius.com>
2013-11-28 10:19:35 +0100

    "Version chiba 2.3"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -10,7 +10,7 @@
 
 return array(
     'name'    => 'Monkey : Novius OS Application Bootstrap',
-    'version' => 'chiba.2.2',
+    'version' => 'chiba.2.3',
     'provider' => array(
         'name' => 'Provider',
     ),

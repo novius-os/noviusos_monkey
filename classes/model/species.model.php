67c2098ab05eb819b57d43d3119bbd76fba69e59
Gilles FELIX <felix@novius.com>
2014-01-16 10:56:47 +0100

    "Init all relation types to empty in models"

--- classes/model/species.model.php
+++ classes/model/species.model.php
@@ -59,6 +59,15 @@ class Model_Species extends \Nos\Orm\Model
         ),
     );
 
+    protected static $_has_one = array();
+    protected static $_belongs_to  = array();
+    protected static $_has_many  = array();
+    protected static $_many_many = array();
+    protected static $_twinnable_has_one = array();
+    protected static $_twinnable_has_many = array();
+    protected static $_twinnable_belongs_to = array();
+    protected static $_twinnable_many_many = array();
+
     protected static $_observers = array(
         'Orm\\Observer_CreatedAt' => array(
             'mysql_timestamp' => true,

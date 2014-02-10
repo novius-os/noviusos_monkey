505a1b13246839d78f2b53814d2c151a29352939
Gilles FELIX <felix@novius.com>
2014-02-10 09:42:58 +0100

    "Dubrovka"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -10,7 +10,7 @@
 
 return array(
     'name'    => 'Monkey : Novius OS Application Bootstrap',
-    'version' => 'chiba.2.4',
+    'version' => 'dubrovka',
     'provider' => array(
         'name' => 'Provider',
     ),
